38d072a6d56801e44fac2a80eb68afdb55392d03
Rononoa13 <sumitluitel42@gmail.com>
2023-03-14 22:16:14 +0545

    Chapter 4 Database complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrage = Migrate(app, db)
 
-from app import routes
+from app import routes, models
 
